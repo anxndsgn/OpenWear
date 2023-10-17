c303079db9e9885a25e71cb4ed409c8c6aa4bc7a
XIN <anxanxx@gmail.com>
2023-10-17 16:07:13 +0800

    update code

--- Arduino Code/RGB/RGB.ino
+++ Arduino Code/RGB/RGB.ino
@@ -1,15 +1,13 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
-
-#define PIN_WS2812B 10  
+ 
 #define NUM_PIXELS 1   
 
-Adafruit_NeoPixel ws2812b(NUM_PIXELS, PIN_WS2812B, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel ws2812b(NUM_PIXELS, NEOPIXEL, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
-  pinMode(5,OUTPUT);
-  digitalWrite(5,HIGH);
+  
   ws2812b.begin();
   ws2812b.clear();  
 
@@ -18,13 +16,19 @@ void setup()
 void loop()
 {
   
-  for(int i=0;i<150;i++){
+  for(int i=0;i<100;i++){
     ws2812b.setPixelColor(0, ws2812b.Color(0, 100, i)); 
     ws2812b.show(); 
     delay(20);
   }
-  for(int i=150;i>0;i--){
-    ws2812b.setPixelColor(0, ws2812b.Color(100, 0, i)); 
+  for(int i=0;i<100;i++){
+    ws2812b.setPixelColor(0, ws2812b.Color(0, i, 100)); 
+    ws2812b.show(); 
+    delay(20);
+  }
+
+  for(int i=100;i>0;i--){
+    ws2812b.setPixelColor(0, ws2812b.Color(i, 100, 0)); 
     ws2812b.show(); 
     delay(20);
   }
